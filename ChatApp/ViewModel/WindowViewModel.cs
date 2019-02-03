a68923fa5332ea96d27d76863411d1363c465c9e
unknown <philip.dein@gmail>
2019-02-03 18:43:08 +0100

    Window can now rezise again

--- ChatApp/ViewModel/WindowViewModel.cs
+++ ChatApp/ViewModel/WindowViewModel.cs
@@ -11,11 +11,81 @@ namespace ChatApp.ViewModel
     {
         private Window window;
 
-        public string Test { get; set; } = "My littel test";
+        private int outerMarginSize = 10;
+        private int windowRadius = 10;
+
+
+        /// <summary>
+        /// Set the size of the area around the border where you can grab the border to resize it
+        /// </summary>
+        public int ResizeBorder { get; set; } = 6;
+
+        public Thickness ResizeBorderThickness
+        {
+            get
+            {
+                return new Thickness(ResizeBorder + OuterMarginSize);
+            }
+        }
+
+
+        /// <summary>
+        /// Set the outer margin of the window to allow for setting a border shadow. If it is maximized it will return null.
+        /// </summary>
+        public int OuterMarginSize
+        {
+            get
+            {
+                return window.WindowState == WindowState.Maximized ? 0 : outerMarginSize;
+            }
+
+            set
+            {
+                outerMarginSize = value;
+            }
+        }
+
+        public Thickness OuterMarginSizeThickness
+        {
+            get
+            {
+                return new Thickness(OuterMarginSize);
+            }
+        }
+
+        public int WindowRadius
+        {
+            get
+            {
+                return window.WindowState == WindowState.Maximized ? 0 : windowRadius;
+            }
+
+            set
+            {
+                windowRadius = value;
+            }
+        }
+
+        public CornerRadius WindowCornerRadius
+        {
+            get
+            {
+                return new CornerRadius(WindowRadius);
+            }
+        }
 
         public WindowViewModel(Window window)
         {
             this.window = window;
+
+            this.window.StateChanged += (sender, e) =>
+            {
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
         }
     }
 }
