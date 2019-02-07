ada0fa9b9fd49c3747b1331b2f93422fa1d3a5f2
ITViking <philip.dein@gmail.com>
2019-02-07 12:55:07 +0100

    No idea why this file seems to be altered

---
+++ ChatApp/Animation/PageAnimations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows;
+
+namespace ChatApp
+{
+    public static class PageAnimations
+    {
+        public static async Task SlideAndFadeInFromRight(this Page page, float seconds)
+        {
+
+            //Just adds a storyboard
+            var sb = new Storyboard();
+            sb.AddSlideFromRight(seconds, page.WindowWidth);
+
+            sb.AddFadeIn(seconds);
+
+
+            sb.Begin(page);
+
+            page.Visibility = Visibility.Visible;
+
+            await Task.Delay((int)(seconds * 1000));
+        }
+
+        public static async Task SlideAndFadeOutToLeft(this Page page, float seconds)
+        {
+
+            //Just adds a storyboard
+            var sb = new Storyboard();
+            sb.AddSlideToLeft(seconds, page.WindowWidth);
+
+            sb.AddFadeOut(seconds);
+
+
+            sb.Begin(page);
+
+            page.Visibility = Visibility.Visible;
+
+            await Task.Delay((int)(seconds * 1000));
+        }
+    }
+}
---
+++ ChatApp/Animation/StoryboardHelpers.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace ChatApp
+{
+    public static class StoryboardHelpers
+    {
+        public static void AddSlideFromRight(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f)
+        {
+            var animation = new ThicknessAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = new Thickness(offset, 0, -offset, 0),
+                To = new Thickness(0),
+                DecelerationRatio = decelerationRatio
+            };
+
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+
+            storyboard.Children.Add(animation);
+        }
+
+        public static void AddSlideToLeft(this Storyboard storyboard, float seconds, double offset, float decelerationRatio = 0.9f)
+        {
+            var animation = new ThicknessAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = new Thickness(0),
+                To = new Thickness(-offset, 0, offset, 0),
+                DecelerationRatio = decelerationRatio
+            };
+
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+
+            storyboard.Children.Add(animation);
+        }
+
+        public static void AddFadeIn(this Storyboard storyboard, float seconds)
+        {
+            var animation = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = 0,
+                To = 1
+            };
+
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Opacity"));
+
+            storyboard.Children.Add(animation);
+        }
+
+        public static void AddFadeOut(this Storyboard storyboard, float seconds)
+        {
+            var animation = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = 1,
+                To = 0
+            };
+
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Opacity"));
+
+            storyboard.Children.Add(animation);
+        }
+    }
+}
